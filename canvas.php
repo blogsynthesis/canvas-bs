73a6cf314a4dbc658073e43cf0eb6a4e5d92b136
Anand Kumar <anand@anandkumar.net>
2015-03-17 20:25:00 +0530

    A custom plugin to get started for beginners.

---
+++ canvas.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Canvas (BlogSynthesis)
+ * Plugin URI: http://www.blogsynthesis.com/custom-wordpress-plugin/
+ * Description: This is the site specific custom plugin.
+ * Version: 11.0.0
+ * Author: Anand Kumar
+ * Author URI: http://www.blogsynthesis.com
+ * License: GPL2
+ */
+
+/** 
+ * To read more about about getting started visit:
+ * http://www.blogsynthesis.com/custom-wordpress-plugin/
+ * OR http://codex.wordpress.org/Writing_a_Plugin
+ * 
+ * START ADDING YOUR CUSTOM CODE BELOW THIS LINE  */
+ 
+ 
